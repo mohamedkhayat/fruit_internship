93e79484b69cb2810b25902fdb8f838bff52f1c0
mohamedkhayat <132056305+mohamedkhayat@users.noreply.github.com>
2025-06-15 16:03:57 +0100

    refactor: integrate training and evaluation into Trainer class, streamline optimizer and scheduler setup

--- src/main.py
+++ src/main.py
@@ -1,18 +1,11 @@
 from omegaconf import DictConfig
 import hydra
-from utils.data import (
-    make_datasets,
-    make_dataloaders,
-    download_dataset
-)
+from utils.data import make_datasets, make_dataloaders, download_dataset, set_transforms
 from models.model_factory import get_model
 import torch
-from utils.trainer import eval
 import torch.nn as nn
-from torch.amp import GradScaler
 import cv2
-from utils.trainer import train, eval, get_optimizer
-from utils.early_stop import EarlyStopping
+from utils.trainer import Trainer
 from tqdm import tqdm
 from utils.logging import (
     initwandb,
@@ -21,12 +14,12 @@ from utils.logging import (
     log_images,
 )
 from utils.general import set_seed
-from torch.optim.lr_scheduler import CosineAnnealingLR, SequentialLR, LinearLR
+
 cv2.setNumThreads(0)
+
 
 @hydra.main(version_base=None, config_path="../conf", config_name="config")
 def main(cfg: DictConfig):
-
     if cfg.download_data:
         download_dataset()
 
@@ -36,7 +29,7 @@ def main(cfg: DictConfig):
     else:
         run = None
         name = get_run_name(cfg)
-    
+
     generator = set_seed(cfg.seed)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -56,101 +49,17 @@ def main(cfg: DictConfig):
         train_ds.lbl2id,
     )  # type:ignore
 
-    train_ds.transforms = transforms["train"]
-    test_ds.transforms = transforms["test"]
-    val_ds.transforms = transforms["test"]
+    train_ds, test_ds, val_ds = set_transforms(train_ds, test_ds, val_ds, transforms)
 
     log_images(run, next(iter(test_dl)), test_ds.id2lbl)
     log_transforms(run, next(iter(train_dl)), (3, 3), train_ds.id2lbl, transforms)
 
-    early_stopping = EarlyStopping(cfg.patience, cfg.delta, "checkpoints", name)
-
-    optimizer = get_optimizer(model, cfg.lr, cfg.lr / cfg.lr_factor, cfg.weight_decay)
-    warmup_scheduler = LinearLR(
-        optimizer, start_factor=0.1, total_iters=cfg.warmup_epochs
+    trainer = Trainer(
+        model, processor, device, cfg, name, run, train_dl, test_dl, val_dl
     )
-
-    main_scheduler = CosineAnnealingLR(
-        optimizer, T_max=cfg.epochs - cfg.warmup_epochs, eta_min=1e-6
-    )
-
-    scheduler = SequentialLR(
-        optimizer,
-        schedulers=[warmup_scheduler, main_scheduler],
-        milestones=[cfg.warmup_epochs],
-    )
-    scaler = GradScaler("cuda")
-
     print("Setup complete.")
-    epoch_pbar = tqdm(total=cfg.epochs, desc="Epochs", position=0, leave=True)
 
-    best_test_map = 0
-
-    for epoch in range(cfg.epochs):
-        epoch_pbar.set_description(f"Epoch {epoch + 1}/{cfg.epochs}")
-
-        train_loss = train(
-            model,
-            device,
-            train_dl,
-            scaler,
-            optimizer,
-            epoch + 1,
-            processor,
-        )
-        tqdm.write(f"\tTrain --- Loss: {train_loss:.4f}")
-
-        test_loss, test_map, test_map50 = eval(
-            model, device, test_dl, epoch + 1, processor
-        )
-        tqdm.write(
-            f"\tEval  --- Loss: {test_loss:.4f}, mAP50-95: {test_map:.4f}, mAP@50 : {test_map50:.4f}"
-        )
-
-        scheduler.step()
-        """
-        epoch_pbar.set_postfix_str(
-            f"Test Loss: {test_loss:.4f}, Test mAP: {test_map:.4f} , Test mAP@50 : {test_map50:.4f}"
-        )
-        """
-
-        epoch_pbar.update(1)
-
-        best_test_map = max(test_map50, best_test_map)
-
-        if cfg.log:
-            run.log(
-                {
-                    "epoch": epoch,
-                    "train/loss": train_loss,
-                    "test/map": test_map,
-                    "test/map 50": test_map50,
-                    "test/loss": test_loss,
-                    "Learning rate": float(f"{scheduler.get_last_lr()[0]:.6f}"),
-                },
-            )
-        if early_stopping(test_map, model):
-            tqdm.write(f"Early stopping triggered at epoch {epoch + 1}.")
-            break
-
-    tqdm.write("Training finished.")
-
-    if cfg.log:
-        run.log({"test/best test map": best_test_map})
-        # log_confusion_matrix(run, y_true, y_pred, labels)
-        model = early_stopping.get_best_model(model)
-
-        val_loss, val_map, val_map50 = eval(
-            model, device, val_dl, epoch + 1, processor
-        )
-
-        run.log({"val/loss": val_loss, "val/map": val_map, "val/map@50": val_map50})
-        run.finish()
-    tqdm.write(
-        f"\tVal  --- Loss: {val_loss:.4f}, mAP50-95: {val_map:.4f}, mAP@50 : {val_map50:.4f}"
-    )
-
-    epoch_pbar.close()
+    trainer.fit()
 
 
 if __name__ == "__main__":
--- src/utils/trainer.py
+++ src/utils/trainer.py
@@ -4,183 +4,284 @@ from torch.amp import GradScaler
 import torchvision
 from tqdm import tqdm
 from torchmetrics.detection.mean_ap import MeanAveragePrecision
+from .early_stop import EarlyStopping
+from torch.optim.lr_scheduler import CosineAnnealingLR, SequentialLR, LinearLR
 
 
-def get_optimizer(model, head_lr=1e-4, backbone_lr=1e-5, weight_decay=1e-4, **kwargs):
-    param_dicts = [
-        {
-            "params": [
-                p
-                for n, p in model.named_parameters()
-                if "backbone" not in n and p.requires_grad
+class Trainer:
+    def __init__(
+        self, model, processor, device, cfg, name, run, train_dl, test_dl, val_dl
+    ):
+        self.model = model
+        self.device = device
+        self.scaler = GradScaler("cuda")
+        self.cfg = cfg
+        self.optimizer = self.get_optimizer()
+        self.processor = processor
+        self.name = name
+        self.early_stopping = EarlyStopping(
+            cfg.patience, cfg.delta, "checkpoints", name
+        )
+        self.scheduler = self.get_scheduler()
+        self.run = run
+        self.train_dl = train_dl
+        self.test_dl = test_dl
+        self.val_dl = val_dl
+
+    def get_scheduler(self):
+        warmup_scheduler = LinearLR(
+            self.optimizer, start_factor=0.1, total_iters=self.cfg.warmup_epochs
+        )
+
+        main_scheduler = CosineAnnealingLR(
+            self.optimizer, T_max=self.cfg.epochs - self.cfg.warmup_epochs, eta_min=1e-6
+        )
+
+        scheduler = SequentialLR(
+            self.optimizer,
+            schedulers=[warmup_scheduler, main_scheduler],
+            milestones=[self.cfg.warmup_epochs],
+        )
+        return scheduler
+
+    def get_optimizer(self):
+        param_dicts = [
+            {
+                "params": [
+                    p
+                    for n, p in self.model.named_parameters()
+                    if "backbone" not in n and p.requires_grad
+                ]
+            },
+            {
+                "params": [
+                    p
+                    for n, p in self.model.named_parameters()
+                    if "backbone" in n and p.requires_grad
+                ],
+                "lr": self.cfg.lr / self.cfg.lr_factor,
+            },
+        ]
+
+        optimizer = torch.optim.AdamW(
+            param_dicts, lr=self.cfg.lr, weight_decay=self.cfg.weight_decay
+        )
+        return optimizer
+
+    def to_coco_dict(self, idx: int, boxes, labels) -> dict:
+        coco_ann = []
+        for box, cls in zip(boxes, labels):
+            x1, y1, x2, y2 = box.tolist()
+            coco_box = [x1, y1, x2 - x1, y2 - y1]
+            area = (x2 - x1) * (y2 - y1)
+            coco_ann.append(
+                {
+                    "category_id": int(cls),
+                    "bbox": coco_box,
+                    "area": area,
+                    "iscrowd": 0,
+                }
+            )
+        return {"image_id": idx, "annotations": coco_ann}
+
+    def move_labels_to_device(self, batch):
+        for lab in batch["labels"]:
+            for k, v in lab.items():
+                lab[k] = v.to(self.device)
+        return batch
+
+    def nested_to_cpu(self, obj):
+        if torch.is_tensor(obj):
+            return obj.cpu()
+        if isinstance(obj, dict):
+            return {k: self.nested_to_cpu(v) for k, v in obj.items()}
+        if isinstance(obj, (list, tuple)):
+            return [self.nested_to_cpu(v) for v in obj]
+        return obj
+
+    def train(
+        self,
+        current_epoch,
+    ):
+        self.model.train()
+        loss = 0.0
+
+        device_str = str(self.device).split(":")[0]
+        progress_bar = tqdm(
+            self.train_dl,
+            desc=f"Epoch {current_epoch} Training",
+            leave=False,
+            bar_format="{l_bar}{bar:30}{r_bar}{bar:-30b}",
+        )
+
+        for batch_idx, (x, y) in enumerate(progress_bar):
+            coco_targets = [
+                self.to_coco_dict(i, t["boxes"], t["labels"]) for i, t in enumerate(y)
             ]
-        },
-        {
-            "params": [
-                p
-                for n, p in model.named_parameters()
-                if "backbone" in n and p.requires_grad
-            ],
-            "lr": backbone_lr,
-        },
-    ]
+            batch = self.processor(
+                images=x,
+                annotations=coco_targets,
+                return_tensors="pt",
+            ).to(self.device)
 
-    optimizer = torch.optim.AdamW(param_dicts, lr=head_lr, weight_decay=weight_decay)
-    return optimizer
+            batch = self.move_labels_to_device(batch)
+            self.optimizer.zero_grad()
 
+            with torch.autocast(device_type=device_str, dtype=torch.float16):
+                out = self.model(**batch)
+                batch_loss = out.loss
 
-def to_coco_dict(idx: int, boxes, labels) -> dict:
-    coco_ann = []
-    for box, cls in zip(boxes, labels):
-        x1, y1, x2, y2 = box.tolist()
-        coco_box = [x1, y1, x2 - x1, y2 - y1]
-        area = (x2 - x1) * (y2 - y1)
-        coco_ann.append(
-            {
-                "category_id": int(cls),
-                "bbox": coco_box,
-                "area": area,
-                "iscrowd": 0,
-            }
+            self.scaler.scale(batch_loss).backward()
+            self.scaler.unscale_(self.optimizer)
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
+            self.scaler.step(self.optimizer)
+            self.scaler.update()
+
+            loss += batch_loss.item()
+
+            current_avg_loss = loss / (batch_idx + 1)
+            progress_bar.set_postfix(
+                {
+                    "Loss": f"{current_avg_loss:.4f}",
+                    "Batch": f"{batch_idx + 1}/{len(self.train_dl)}",
+                }
+            )
+
+        loss /= len(self.train_dl)
+
+        tqdm.write(f"\tTrain --- Loss: {loss:.4f}")
+
+        return loss
+
+    @torch.no_grad()
+    def eval(self, test_dl, current_epoch):
+        self.model.eval()
+        metric = MeanAveragePrecision(
+            box_format="xyxy",
+            average="macro",
+            max_detection_thresholds=[1, 10, 100],
+            iou_thresholds=None,
+            class_metrics=True,
         )
-    return {"image_id": idx, "annotations": coco_ann}
+        metric.warn_on_many_detections = False
+        loss = 0.0
 
+        device_str = str(self.device).split(":")[0]
 
-def move_labels_to_device(batch, device):
-    for lab in batch["labels"]:
-        for k, v in lab.items():
-            lab[k] = v.to(device)
-    return batch
-
-
-def train(
-    model: nn.Module,
-    device: torch.device,
-    train_dl,
-    scaler: GradScaler,
-    optimizer,
-    current_epoch,
-    processor,
-):
-    model.train()
-    loss = 0.0
-
-    device_str = str(device).split(":")[0]
-    progress_bar = tqdm(
-        train_dl,
-        desc=f"Epoch {current_epoch} Training",
-        leave=False,
-        bar_format="{l_bar}{bar:30}{r_bar}{bar:-30b}",
-    )
-
-    for batch_idx, (x, y) in enumerate(progress_bar):
-        coco_targets = [
-            to_coco_dict(i, t["boxes"], t["labels"]) for i, t in enumerate(y)
-        ]
-        batch = processor(
-            images=x,
-            annotations=coco_targets,
-            return_tensors="pt",
-        ).to(device)
-        batch = move_labels_to_device(batch, device)
-        optimizer.zero_grad()
-
-        with torch.autocast(device_type=device_str, dtype=torch.float16):
-            out = model(**batch)
-            batch_loss = out.loss
-
-        scaler.scale(batch_loss).backward()
-        scaler.unscale_(optimizer)
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-        scaler.step(optimizer)
-        scaler.update()
-
-        loss += batch_loss.item()
-
-        current_avg_loss = loss / (batch_idx + 1)
-        progress_bar.set_postfix(
-            {
-                "Loss": f"{current_avg_loss:.4f}",
-                "Batch": f"{batch_idx + 1}/{len(train_dl)}",
-            }
+        progress_bar = tqdm(
+            test_dl,
+            desc=f"Epoch {current_epoch} Evaluating",
+            leave=False,
+            bar_format="{l_bar}{bar:30}{r_bar}{bar:-30b}",
         )
 
-    loss /= len(train_dl)
-    return loss
+        for batch_idx, (x, y) in enumerate(progress_bar):
+            coco_targets = [
+                self.to_coco_dict(i, t["boxes"], t["labels"]) for i, t in enumerate(y)
+            ]
+            batch = self.processor(
+                images=x,
+                annotations=coco_targets,
+                return_tensors="pt",
+            ).to(self.device)
 
+            batch = self.move_labels_to_device(batch)
 
-@torch.no_grad()
-def eval(model: nn.Module, device, test_dl, current_epoch, processor):
-    model.eval()
-    metric = MeanAveragePrecision(
-        box_format="xyxy",
-        average="macro",
-        max_detection_thresholds=[1, 10, 100],
-        iou_thresholds=None,
-        class_metrics=True,
-    )
-    metric.warn_on_many_detections = False
-    loss = 0.0
+            with torch.autocast(device_type=device_str, dtype=torch.float16):
+                out = self.model(**batch)
+                batch_loss = out.loss
 
-    device_str = str(device).split(":")[0]
+            loss += batch_loss.item()
 
-    progress_bar = tqdm(
-        test_dl,
-        desc=f"Epoch {current_epoch} Evaluating",
-        leave=False,
-        bar_format="{l_bar}{bar:30}{r_bar}{bar:-30b}",
-    )
+            current_avg_loss = loss / (batch_idx + 1)
 
-    for batch_idx, (x, y) in enumerate(progress_bar):
-        coco_targets = [
-            to_coco_dict(i, t["boxes"], t["labels"]) for i, t in enumerate(y)
-        ]
-        batch = processor(
-            images=x,
-            annotations=coco_targets,
-            return_tensors="pt",
-        ).to(device)
+            progress_bar.set_postfix(
+                {
+                    "Loss": f"{current_avg_loss:.4f}",
+                    "Batch": f"{batch_idx + 1}/{len(test_dl)}",
+                }
+            )
+            sizes = torch.tensor([[img.shape[0], img.shape[1]] for img in x]).to(
+                self.device
+            )
+            preds = self.processor.post_process_object_detection(
+                out, threshold=0.001, target_sizes=sizes
+            )
+            for p in preds:
+                topk = p["scores"].argsort(descending=True)[:300]
+                for k in ("boxes", "scores", "labels"):
+                    p[k] = p[k][topk]
 
-        batch = move_labels_to_device(batch, device)
+            preds = self.nested_to_cpu(preds)
+            metric.update(preds, y)
 
-        with torch.autocast(device_type=device_str, dtype=torch.float16):
-            out = model(**batch)
-            batch_loss = out.loss
+        stats = metric.compute()
+        metric.reset()
 
-        loss += batch_loss.item()
+        loss /= len(test_dl)
+        test_map, test_map50 = stats["map"].item(), stats["map_50"].item()
 
-        current_avg_loss = loss / (batch_idx + 1)
-
-        progress_bar.set_postfix(
-            {
-                "Loss": f"{current_avg_loss:.4f}",
-                "Batch": f"{batch_idx + 1}/{len(test_dl)}",
-            }
+        tqdm.write(
+            f"\tEval  --- Loss: {loss:.4f}, mAP50-95: {test_map:.4f}, mAP@50 : {test_map50:.4f}"
         )
-        sizes = torch.tensor([[img.shape[0], img.shape[1]] for img in x]).to(device)
-        preds = processor.post_process_object_detection(
-            out, threshold=0.001, target_sizes=sizes
+
+        return loss, test_map, test_map50
+
+    def fit(self):
+        epoch_pbar = tqdm(total=self.cfg.epochs, desc="Epochs", position=0, leave=True)
+
+        best_test_map = 0
+
+        for epoch in range(self.cfg.epochs):
+            epoch_pbar.set_description(f"Epoch {epoch + 1}/{self.cfg.epochs}")
+
+            train_loss = self.train(
+                epoch + 1,
+            )
+
+            test_loss, test_map, test_map50 = self.eval(self.test_dl, epoch + 1)
+
+            self.scheduler.step()
+
+            epoch_pbar.update(1)
+
+            best_test_map = max(test_map50, best_test_map)
+
+            if self.cfg.log:
+                self.run.log(
+                    {
+                        "epoch": epoch,
+                        "train/loss": train_loss,
+                        "test/map": test_map,
+                        "test/map 50": test_map50,
+                        "test/loss": test_loss,
+                        "Learning rate": float(
+                            f"{self.scheduler.get_last_lr()[0]:.6f}"
+                        ),
+                    },
+                )
+            if self.early_stopping(test_map, self.model):
+                tqdm.write(f"Early stopping triggered at epoch {epoch + 1}.")
+                break
+
+        tqdm.write("Training finished.")
+
+        if self.cfg.log:
+            self.run.log({"test/best test map": best_test_map})
+            # log_confusion_matrix(run, y_true, y_pred, labels)
+            model = self.early_stopping.get_best_model(model)
+
+            val_loss, val_map, val_map50 = self.eval(
+                self.val_dl, epoch + 1
+            )
+
+            self.run.log(
+                {"val/loss": val_loss, "val/map": val_map, "val/map@50": val_map50}
+            )
+            self.run.finish()
+
+        tqdm.write(
+            f"\tVal  --- Loss: {val_loss:.4f}, mAP50-95: {val_map:.4f}, mAP@50 : {val_map50:.4f}"
         )
-        for p in preds:
-            topk = p["scores"].argsort(descending=True)[:300]
-            for k in ("boxes", "scores", "labels"):
-                p[k] = p[k][topk]
 
-        preds = nested_to_cpu(preds)
-        metric.update(preds, y)
-
-    stats = metric.compute()
-    metric.reset()
-
-    loss /= len(test_dl)
-    return loss, stats["map"].item(), stats["map_50"].item()
-
-
-def nested_to_cpu(obj):
-    if torch.is_tensor(obj):
-        return obj.cpu()
-    if isinstance(obj, dict):
-        return {k: nested_to_cpu(v) for k, v in obj.items()}
-    if isinstance(obj, (list, tuple)):
-        return [nested_to_cpu(v) for v in obj]
-    return obj
+        epoch_pbar.close()
