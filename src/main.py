6ec02ad74077f274c7edf5e8f6be0989e9ac1ba5
mohamedkhayat <132056305+mohamedkhayat@users.noreply.github.com>
2025-06-16 00:53:47 +0100

    refactor: remove unnecessary blank line before main function definition

--- src/main.py
+++ src/main.py
@@ -16,7 +16,6 @@ from utils.general import set_seed
 
 cv2.setNumThreads(0)
 
-
 @hydra.main(version_base=None, config_path="../conf", config_name="config")
 def main(cfg: DictConfig):
     if cfg.log:
