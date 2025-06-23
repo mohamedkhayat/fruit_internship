1f385ab5a2f8339bdb3c61a714f3d8de43f60e6e
mohamedkhayat <132056305+mohamedkhayat@users.noreply.github.com>
2025-06-23 20:16:29 +0100

    added mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -6,10 +6,13 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Fruit Internship'
-copyright = '2025, Mohamed Khayat'
-author = 'Mohamed Khayat'
-release = '0.0.1'
+import pathlib
+import sys
+
+project = "Fruit Internship"
+copyright = "2025, Mohamed Khayat"
+author = "Mohamed Khayat"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -20,17 +23,21 @@ extensions = [
     "sphinx_autodoc_typehints",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+autodoc_mock_imports = [
+    "omegaconf",
+    "torch",
+    "albumentations",
+    "numpy",
+    "matplotlib",
+]
 
-import os
-import pathlib
-import sys
 PROJECT_ROOT = pathlib.Path(__file__).resolve().parents[2]  # two levels up
 sys.path.insert(0, str(PROJECT_ROOT))
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
