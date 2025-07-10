5b2fd05d5a90fdfff99204229c3a5a59c61dab24
mohamedkhayat <132056305+mohamedkhayat@users.noreply.github.com>
2025-07-10 18:22:02 +0100

    fix: increase batch size in make_dataloaders for test and validation data loaders

--- src/fruit_project/utils/data.py
+++ src/fruit_project/utils/data.py
@@ -207,7 +207,7 @@ def make_dataloaders(
 
     test_dl = DataLoader(
         test_ds,
-        batch_size=1,  # cfg.step_batch_size,
+        batch_size=4,  # cfg.step_batch_size,
         num_workers=cfg.num_workers,
         persistent_workers=True,
         pin_memory=True,
@@ -218,7 +218,7 @@ def make_dataloaders(
 
     val_dl = DataLoader(
         val_ds,
-        batch_size=1,  # cfg.step_batch_size,
+        batch_size=4,  # cfg.step_batch_size,
         num_workers=cfg.num_workers,
         persistent_workers=True,
         pin_memory=True,
