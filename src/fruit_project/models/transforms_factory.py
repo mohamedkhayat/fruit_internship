badb58c7759c69f91b7ea8830f0a7fbe2e069295
mohamedkhayat <132056305+mohamedkhayat@users.noreply.github.com>
2025-07-12 18:33:58 +0100

    fix: add ConstrainedCoarseDropout transformation for enhanced data augmentation

--- src/fruit_project/models/transforms_factory.py
+++ src/fruit_project/models/transforms_factory.py
@@ -36,6 +36,14 @@ def get_transforms(cfg: DictConfig, id2label: Dict[int, str]) -> Dict[str, A.Com
                 ],
                 p=0.2,
             ),
+            A.ConstrainedCoarseDropout(
+                num_holes_range=(1, 3),
+                hole_height_range=(0.2, 0.3),
+                hole_width_range=(0.2, 0.3),
+                bbox_labels=[k for k in id2label.keys()],
+                fill=0,
+                p=0.2,
+            ),
             A.Perspective(p=0.1),
             A.OneOf(
                 [
