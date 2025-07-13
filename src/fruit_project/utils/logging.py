316cf89614fb712263e22b3dc685e2bff230bb11
mohamedkhayat <132056305+mohamedkhayat@users.noreply.github.com>
2025-07-13 02:29:36 +0000

    fix: typo

--- src/fruit_project/utils/logging.py
+++ src/fruit_project/utils/logging.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2025 Mohamed Khayat
-# SPDX-License-Identifier: AGPL-3.0-or-later for me for every file
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
 from collections import Counter
 import numpy as np
