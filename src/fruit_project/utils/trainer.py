fix: simplify evaluation metrics calculation and remove unused detection metrics

src/fruit_project/utils/trainer.py
@@ -375,7 +375,6 @@ class Trainer:
         self.model.eval()
         self.map_evaluator.map_metric.reset()
         self.map_evaluator.map_50_metric.reset()
-        self.map_evaluator.det_metric.clear_stats()
         epoch_loss = {"loss": 0.0}
         epoch_loss.update({k: 0.0 for k in ["class_loss", "bbox_loss", "giou_loss"]})
 
@@ -432,35 +431,12 @@ class Trainer:
                 }
             )
             if calc_cm and cm:
-                sizes = torch.stack(
-                    [t["size"].clone().detach() for t in batch["labels"]]
-                )
-
-                preds = self.processor.post_process_object_detection(
-                    out, threshold=0.01, target_sizes=sizes
-                )
+                preds = self.processor.post_process_object_detection(...)
                 preds = self.nested_to_cpu(preds)
-                for preds_i, targets_i in zip(
-                    batch_preds_processed, batch_targets_processed
-                ):
-                    dets = torch.cat(
-                        [
-                            preds_i["boxes"],
-                            preds_i["scores"].unsqueeze(1),
-                            preds_i["labels"].unsqueeze(1),
-                        ],
-                        dim=1,
-                    )
-                    labs = torch.cat(
-                        [targets_i["labels"].unsqueeze(1), targets_i["boxes"]], dim=1
-                    )
-                    self.map_evaluator.det_metric.update_stats(dets, labs)
-
                 targets_for_cm = self.format_targets_for_cm(batch["labels"])
                 cm.update(preds, targets_for_cm)
 
         tqdm.write("Computing mAP metrics")
-        """"
         map_50_95_metrics = self.map_evaluator.map_metric.compute()
         test_map = map_50_95_metrics.get("map", 0.0)
         test_map50 = map_50_95_metrics.get("map_50", 0.0)
@@ -487,35 +463,6 @@ class Trainer:
             "precision": overall_precision,
             "recall": overall_recall,
         }
-        """
-        det_results = self.map_evaluator.det_metric.process()
-        test_map = det_results["map"]
-        test_map50 = det_results["map50"]
-
-        precision_pc = torch.tensor(det_results["p"], device=self.device)
-        recall_pc = torch.tensor(det_results["r"], device=self.device)
-        map50_pc = torch.tensor(
-            self.map_evaluator.det_metric.box.ap50, device=self.device
-        )
-
-        present = torch.tensor(
-            self.map_evaluator.det_metric.ap_class_index,
-            device=self.device,
-            dtype=torch.long,
-        )
-
-        overall_precision = precision_pc[present].mean().item() if len(present) else 0.0
-        overall_recall = recall_pc[present].mean().item() if len(present) else 0.0
-
-        test_metrics = {
-            "map@50:95": test_map,
-            "map@50": test_map50,
-            "map@50_per_class": map50_pc,
-            "precision_per_class": precision_pc,
-            "recall_per_class": recall_pc,
-            "precision": overall_precision,
-            "recall": overall_recall,
-        }
 
         num_batches = len(test_dl)
         epoch_loss = {k: v / num_batches for k, v in epoch_loss.items()}
@@ -523,9 +470,7 @@ class Trainer:
         tqdm.write(
             f"\tEval  --- Loss: {epoch_loss['loss']:.4f}, Class Loss : {epoch_loss['class_loss']:.4f}, Bbox Loss : {epoch_loss['bbox_loss']:.4f}, Giou Loss : {epoch_loss['giou_loss']:.4f}"
         )
-        tqdm.write(
-            f"\tEval  --- mAP50-95: {test_map:.4f}, mAP@50 : {test_map50:.4f}"
-        )
+        tqdm.write(f"\tEval  --- mAP50-95: {test_map:.4f}, mAP@50 : {test_map50:.4f}")
 
         tqdm.write("\t--- Per-class mAP@50 ---")
         class_names = test_dl.dataset.labels
@@ -617,7 +562,7 @@ class Trainer:
         tqdm.write("done saving checkpoint")
         return path
 
-    def _load_checkpoint(self, path: str, model_only : bool = True) -> None:
+    def _load_checkpoint(self, path: str, model_only: bool = True) -> None:
         """
         Loads a checkpoint and restores the state of the model, optimizer, scheduler, and scaler.
 