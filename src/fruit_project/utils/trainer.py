5194734b56454e188c3d838151bf07c01fe5e398
mohamedkhayat <132056305+mohamedkhayat@users.noreply.github.com>
2025-07-16 23:21:36 +0000

    fix: update warmup scheduler to use lin_start_factor from configuration

--- src/fruit_project/utils/trainer.py
+++ src/fruit_project/utils/trainer.py
@@ -90,7 +90,7 @@ class Trainer:
         """
         if self.cfg.phase == 1:
             warmup_scheduler = LinearLR(
-                self.optimizer, start_factor=cfg.lin_start_factor, total_iters=self.cfg.warmup_epochs
+                self.optimizer, start_factor=self.cfg.lin_start_factor, total_iters=self.cfg.warmup_epochs
             )
 
             main_scheduler = CosineAnnealingLR(
